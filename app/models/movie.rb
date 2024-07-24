89c940bd549b0b43bd5217f139e0745eee08a9da
wsnc0 <celest_ng@mymail.sutd.edu.sg>
2024-07-25 00:20:43 +0800

    Initial commit

---
+++ app/models/movie.rb
@@ -0,0 +1,9 @@
+class Movie < ActiveRecord::Base
+
+  #### Part 1 ####
+  # implement this method. Remeber to exclude [self]
+  # (the current movie) from your return value
+  def others_by_same_director
+    # Your code here #
+  end
+end
